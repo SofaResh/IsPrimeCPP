28871dc9b495abdc13d910d3ace4f2bcc8b824c3
SofaResh <reshetovasofa@mail.ru>
2024-02-26 18:08:50 +0300

    Добавить тесты

--- Tests/IsPrimeTests.cpp
+++ Tests/IsPrimeTests.cpp
@@ -1,7 +1,28 @@
 #include "gtest/gtest.h"
 #include "IsPrime.h"
 
-TEST(IsPrimeTest, PrimeNumberTrue){
+TEST(IsPrimeTest, PrimeNumberTrue1) {
     EXPECT_TRUE(IsPrime::isPrime(2));
+}
+
+TEST(IsPrimeTest, PrimeNumberTrue2) {
+    EXPECT_TRUE(IsPrime::isPrime(5));
+}
+
+TEST(IsPrimeTest, PrimeNumberTrue3) {
+    EXPECT_TRUE(IsPrime::isPrime(11));
+}
+
+TEST(IsPrimeTest, PrimeNumberFalse1) {
+    EXPECT_FALSE(IsPrime::isPrime(4));
+
+}
+
+TEST(IsPrimeTest, PrimeNumberFalse2) {
+    EXPECT_FALSE(IsPrime::isPrime(12));
+}
+
+TEST(IsPrimeTest, PrimeNumberFalse3) {
+    EXPECT_FALSE(IsPrime::isPrime(16));
 }
 
